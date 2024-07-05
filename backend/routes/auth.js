1e9cb3e83a5d2e7d13a114f32cdb3a30bc7a8330
rachagar20 <rach.agar20@gmail.com>
2024-07-06 01:21:38 +0530

    changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -36,7 +36,13 @@ router.post("/login",async (req,res)=>{
         }
         const token=jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.JWT_SECRET,{expiresIn:"3d"})
         const {password,...info}=user._doc
-        res.cookie("token",token).status(200).json(info)
+        res.cookie("token",token,
+            {
+                httpOnly: true,
+                secure: true, // Set to true if using HTTPS
+                sameSite: 'None', // Required for cross-site cookies
+              }
+        ).status(200).json(info)
 
     }
     catch(err){
